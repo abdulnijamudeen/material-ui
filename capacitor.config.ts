5000834aad3917c9d3cefc93641cdc2cb6eb912e
Nizam Abdul <abdulnijamudeen@gmail.com>
2022-07-30 07:46:21 +0530

    Live reload impl

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,11 @@ const config: CapacitorConfig = {
   appId: 'com.nizam.materialui',
   appName: 'material-ui',
   webDir: 'dist/material-ui',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    url: 'http://192.168.1.10:4200',
+    cleartext: true
+  }
 };
 
 export default config;
